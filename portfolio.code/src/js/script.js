3350837f3ed6bf6f6a7c0b9008558b6cb56c2955
mochitsuki-mo <mo3ov.877@gmail.com>
2024-04-01 09:12:03 +0900

    portfolio

--- portfolio.code/src/js/script.js
+++ portfolio.code/src/js/script.js
@@ -29,3 +29,21 @@ $(function () {
     });
   });
 });
+
+// アコーディオン　コピペ修正前
+$(function () {
+  //.accordion_oneの中の.accordion_headerがクリックされたら
+  $(".answer-content .answer-accordion .accordion_header").click(function () {
+    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+    $(this).next(".accordion_inner").slideToggle();
+    $(this).toggleClass("open");
+    //クリックされた.accordion_oneの中の.accordion_header以外の.accordion_oneの中の.accordion_headerに隣接する.accordion_oneの中の.accordion_innerを閉じる
+    $(".answer-content .answer-accordion .accordion_header")
+      .not($(this))
+      .next(".answer-accordion .accordion_inner")
+      .slideUp();
+    $(".answer-content .answer-accordion .accordion_header")
+      .not($(this))
+      .removeClass("open");
+  });
+});
