0ec4fe7b7df72b58d09f55fa2dc495942e0b220f
mochitsuki-mo <mo3ov.877@gmail.com>
2024-04-05 16:00:56 +0900

    portfolio

--- portfolio.code/src/js/script.js
+++ portfolio.code/src/js/script.js
@@ -1,3 +1,15 @@
+// loading
+$(function () {
+  function end_loader() {
+    $(".load").fadeOut(800);
+  }
+  $(window).on("load", function () {
+    setTimeout(function () {
+      end_loader();
+    }, 3000);
+  });
+});
+
 // hamburger
 
 $(function () {
@@ -5,6 +17,58 @@ $(function () {
     $(".hamburger").toggleClass("open");
     $(".header-nav").fadeToggle();
   });
+});
+
+//
+
+// スクロールするとロゴの色変更
+$(function () {
+  $(window).on("scroll", function () {
+    const sliderHeight = $(".page-kv").height();
+    if (sliderHeight - 30 < $(this).scrollTop()) {
+      $(".hamburger.other").addClass("hamColorScroll");
+    } else {
+      $(".hamburger.other").removeClass("hamColorScroll");
+    }
+  });
+});
+
+// mv g-sap
+window.addEventListener("DOMContentLoaded", () => {
+  gsap
+    .timeline({ repeat: -1, repeatDelay: 0.5 })
+    .from(".rect-1", { y: -32, opacity: 0, duration: 0.5 })
+    .from(".rect-2", { y: 32, opacity: 0, duration: 0.5 })
+    .from(".rect-3", { y: -32, opacity: 0, duration: 0.5 })
+    .from(".rect-4", { y: 32, opacity: 0, duration: 0.5 })
+    .from(".rect-5", { y: -32, opacity: 0, duration: 0.5 })
+    .to(".rect-1", { y: -32, opacity: 0, duration: 0.5 }, "+=0.5")
+    .to(".rect-2", { y: 32, opacity: 0, duration: 0.5 }, "-=0.4") // 0.4秒開始を早める
+    .to(".rect-3", { y: -32, opacity: 0, duration: 0.5 }, "-=0.4")
+    .to(".rect-4", { y: 32, opacity: 0, duration: 0.5 }, "-=0.4")
+    .to(".rect-5", { y: -32, opacity: 0, duration: 0.5 }, "-=0.4");
+});
+
+// mv slide
+document.addEventListener("DOMContentLoaded", () => {
+  const figure = document.querySelector(".mv-img figure");
+  let currentRotation = 0; // 現在の回転角度
+  const rotationStep = 72; // 画像1枚分の回転角度（360度 / 画像の枚数）
+  const rotationInterval = 2000; // 自動回転の間隔（ミリ秒）
+
+  // 回転処理を実行する関数
+  function rotateSlider() {
+    currentRotation += rotationStep;
+    figure.style.transform = `rotateY(${currentRotation}deg)`;
+  }
+  // 自動回転を開始する
+  let autoRotate = setInterval(rotateSlider, rotationInterval);
+
+  // 自動回転をリセットする関数
+  function resetAutoRotate() {
+    clearInterval(autoRotate);
+    autoRotate = setInterval(rotateSlider, rotationInterval);
+  }
 });
 
 // fade in
@@ -53,12 +117,12 @@ $(function () {
 });
 
 // スクロールしたら色が出る
-$(function () {
-  $(".js-filter").on("invert", function (event, isInView) {
-    if (isInView) {
-      $(this).addClass("invert");
-    } else {
-      $(this).removeClass("invert");
-    }
-  });
-});
+// $(function () {
+//   $(".js-filter").on("invert", function (event, isInView) {
+//     if (isInView) {
+//       $(this).addClass("invert");
+//     } else {
+//       $(this).removeClass("invert");
+//     }
+//   });
+// });
